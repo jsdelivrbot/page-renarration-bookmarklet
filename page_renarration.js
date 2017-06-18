020c2fdedcbbb19acd05747920dde09d9e791b7d
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-06-19 05:05:56 +0530

    made changes

--- page_renarration.js
+++ page_renarration.js
@@ -18,73 +18,75 @@ function annoletContainer(){
     script_tag.type = "text/javascript";
     script_tag.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"; 
     document.getElementsByTagName('head')[0].appendChild(script_tag);
-    
-    function getText(){
-        var url = '';
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-        xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-        xhr.send(null);
-        xhr.onreadystatechange = function() {
-            if (this.readyState==4 && this.status==200) {
-                container.innerHTML = this.responseText;          
-            }
-        }
-    }
 
     //injecting html code
-    container.innerHTML = "<h4 id='annolet-header'>Page Renarration...!</h4>"+
-    "<ul id='annolet-menu' >"+
-        "<li class='annolet-element'>"+
-            "<button id='disable-css' class='annolet-button'>No CSS</button>"+
-        "</li>"+
-        "<li class='annolet-element'>"+
-            "<button id='zapper' class='annolet-button' >Zapper</button>"+
-        "</li>"+
-        "<li class='annolet-element'>"+
-            "<button id='modify-content' class='annolet-button' >Modify Content</button>"+
-        "</li>"+
-        "<li class='annolet-element' >"+
-            "<button id='highlighter-btn' class='annolet-button' >Highlighter</button>"+
-        "</li>"+
-        "<li class='annolet-element' >"+
-            "<button id='phonetics-btn' class='annolet-button' >Phonetics</button>"+
-        "</li>"+
-        "<li class='annolet-element'>"+
-            "<select class='select-menu' id='select-lang' >"+
-                "<option value='hi' >Hindi</option>"+
-                "<option value='te' >Telugu</option>"+
-                "<option value='ta' >Tamil</option>"+
-                "<option value='ml' >Malayalam</option>"+
-                "<option value='ja' >Japanese</option>"+
-                "<option value='zh-Hans' >Chinese(Simplified)</option>"+
-            "</select>"+"<br>"+
-            "<h6 style='color:orange;'>Translate Text</h6>"+
-        "</li>"+
-        "<li class='annolet-element'>"+
-            "<select class='select-menu' id='select-theme'>"+
-                "<option value='switch1' >Theme1</option>"+
-                "<option value='switch2' >Theme2</option>"+
-                "<option value='switch3' >Theme3</option>"+
-            "</select>"+"<br>"+
-            "<h6 style='color:orange;'>Switch CSS</h6>"+
-        "</li>"+
-        "<li class='annolet-element'>"+
-            "<select class='select-menu' id='select-content'>"+
-                "<option value='show-links' >Show Links</option>"+
-                "<option value='show-text' >Show Text</option>"+
-                "<option value='show-images' >Show Images</option>"+
-            "</select>"+"<br>"+
-            "<h6 style='color:orange;'>Webpage Stripper</h6>"+
-        "</li>"+
-        "<li class='annolet-element'>"+
-            "<select class='select-menu' >"+
-                "<option id='increase-font' >Increase Font</option>"+
-                "<option id='decrease-font' >Decrease Font</option>"+
-            "</select>"+"<br>"+
-            "<h6 style='color:orange;'>Visibility</h6>"+
-        "</li>"+
-    "</ul>";
+    // container.innerHTML = "<h4 id='annolet-header'>Page Renarration...!</h4>"+
+    // "<ul id='annolet-menu' >"+
+    //     "<li class='annolet-element'>"+
+    //         "<button id='disable-css' class='annolet-button'>No CSS</button>"+
+    //     "</li>"+
+    //     "<li class='annolet-element'>"+
+    //         "<button id='zapper' class='annolet-button' >Zapper</button>"+
+    //     "</li>"+
+    //     "<li class='annolet-element'>"+
+    //         "<button id='modify-content' class='annolet-button' >Modify Content</button>"+
+    //     "</li>"+
+    //     "<li class='annolet-element' >"+
+    //         "<button id='highlighter-btn' class='annolet-button' >Highlighter</button>"+
+    //     "</li>"+
+    //     "<li class='annolet-element' >"+
+    //         "<button id='phonetics-btn' class='annolet-button' >Phonetics</button>"+
+    //     "</li>"+
+    //     "<li class='annolet-element'>"+
+    //         "<select class='select-menu' id='select-lang' >"+
+    //             "<option value='hi' >Hindi</option>"+
+    //             "<option value='te' >Telugu</option>"+
+    //             "<option value='ta' >Tamil</option>"+
+    //             "<option value='ml' >Malayalam</option>"+
+    //             "<option value='ja' >Japanese</option>"+
+    //             "<option value='zh-Hans' >Chinese(Simplified)</option>"+
+    //         "</select>"+"<br>"+
+    //         "<h6 style='color:orange;'>Translate Text</h6>"+
+    //     "</li>"+
+    //     "<li class='annolet-element'>"+
+    //         "<select class='select-menu' id='select-theme'>"+
+    //             "<option value='switch1' >Theme1</option>"+
+    //             "<option value='switch2' >Theme2</option>"+
+    //             "<option value='switch3' >Theme3</option>"+
+    //         "</select>"+"<br>"+
+    //         "<h6 style='color:orange;'>Switch CSS</h6>"+
+    //     "</li>"+
+    //     "<li class='annolet-element'>"+
+    //         "<select class='select-menu' id='select-content'>"+
+    //             "<option value='show-links' >Show Links</option>"+
+    //             "<option value='show-text' >Show Text</option>"+
+    //             "<option value='show-images' >Show Images</option>"+
+    //         "</select>"+"<br>"+
+    //         "<h6 style='color:orange;'>Webpage Stripper</h6>"+
+    //     "</li>"+
+    //     "<li class='annolet-element'>"+
+    //         "<select class='select-menu' >"+
+    //             "<option id='increase-font' >Increase Font</option>"+
+    //             "<option id='decrease-font' >Decrease Font</option>"+
+    //         "</select>"+"<br>"+
+    //         "<h6 style='color:orange;'>Visibility</h6>"+
+    //     "</li>"+
+    // "</ul>";
+    //container.innerHTML = getText();
+    getText()
+}
+
+function getText(){
+    var url = 'https://cdn.rawgit.com/sadhanareddy/page-renarration-bookmarklet/94b04819/page_renarration.txt';
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    //xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhr.send(null);
+    xhr.onreadystatechange = function() {
+        if (this.readyState==4 && this.status==200) {
+            container.innerHTML = this.responseText;          
+        }
+    }
 }
 
 

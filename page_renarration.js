af80466bfe993e1fb5a0b0e5579dbbaec249bd4a
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-06-26 15:24:54 +0530

    made changes

--- page_renarration.js
+++ page_renarration.js
@@ -255,7 +255,7 @@ function alternateStylesheets(){
     //appending a CSS alternate stylesheets to head element of a webpage.
     var i= 0;
     var style_sheets = 3; 
-    var css_themes =['https://cdn.rawgit.com/sadhanareddy/page-renarration-bookmarklet/360a1042/css/switch1.css',
+    var css_themes =['https://cdn.rawgit.com/sadhanareddy/page-renarration-bookmarklet/54d9629b/css/switch1.css',
     'https://cdn.rawgit.com/sadhanareddy/page-renarration-bookmarklet/360a1042/css/switch2.css',
     'https://cdn.rawgit.com/sadhanareddy/page-renarration-bookmarklet/360a1042/css/switch3.css'];
     var link_title =['switch1', 'switch2', 'switch3'];
